8ca840312f051f0918f7b73f4a5a61a0273d4d32
zapline <278998871@qq.com>
2017-06-15 09:13:22 +0800

    * 文本转码

--- paradox_mate/FormMain.cs
+++ paradox_mate/FormMain.cs
@@ -34,29 +34,69 @@ namespace paradox_mate
             var path = textBoxPath.Text;
             if (path.Count() > 0)
             {
+                var unicode = Encoding.Unicode;
+                var iso = Encoding.GetEncoding("iso-8859-1");
+                var utf8 = Encoding.UTF8;
+                var gbk = Encoding.GetEncoding("GBK");
                 try
                 {
                     var charSet = new HashSet<char>();
                     var files = PathUtil.ListFile(path).Where(x => x.EndsWith("english.yml"));
                     foreach (var f in files)
                     {
-                        var unicode = Encoding.Unicode;
-                        var u8Bytes = TextUtil.ReadFile(f);
-                        byte[] uBytes = Encoding.Convert(Encoding.UTF8, unicode, u8Bytes);
-                        string sss = unicode.GetString(uBytes);
-                        char[] uChars = new char[unicode.GetCharCount(uBytes, 0, uBytes.Length)];
-                        unicode.GetChars(uBytes, 0, uBytes.Length, uChars, 0);
-                        foreach (var c in uChars)
-                        {
-                            if (c > 255)
-                            {
-                                charSet.Add(c);
-                            }
-                        }
 
-                        byte[] isoBytes = Encoding.Convert(Encoding.ASCII, Encoding.UTF8, u8Bytes);
-                        string uf8converted = Encoding.UTF8.GetString(isoBytes);
+                        var u8Bytes = TextUtil.ReadFile(f);
+                        var u8Str = Encoding.UTF8.GetString(u8Bytes);
+                        // 原文本是utf8
+                        var i = u8Str.IndexOf('\"');
+                        while (i != -1)
+                        {
+                            var j = u8Str.IndexOf('\"', i + 1);
+                            if (j == -1) break;
+                            var subStr = u8Str.Substring(i + 1, j - i -1);
+                            if (subStr.Count() > 0)
+                            {
+                                var bs = utf8.GetBytes(subStr);
+                                // 原版exe处理是先utf8转unicode，再转latin1
+                                byte[] uBytes = Encoding.Convert(utf8, unicode, bs);
+                                byte[] latin1Bytes = Encoding.Convert(unicode, iso, uBytes);
+                                var str1 = iso.GetString(latin1Bytes);
+
+                                // 汉化版exe是gbk伪装成latin1
+                                byte[] uBytes2 = Encoding.Convert(gbk, unicode, latin1Bytes);
+                                var str2 = unicode.GetString(uBytes2);
+
+                                char[] uChars = new char[unicode.GetCharCount(uBytes2, 0, uBytes2.Length)];
+                                unicode.GetChars(uBytes2, 0, uBytes2.Length, uChars, 0);
+                                foreach (var c in uChars)
+                                {
+                                    if (c > 255)
+                                    {
+                                        charSet.Add(c);
+                                    }
+                                }
+                            }
+                            i = u8Str.IndexOf('\"', j + 1);
+                        }
                     }
+                    string outStr = "";
+                    var output = Path.Combine(path, "hehe.txt");
+                    FileStream fs = new FileStream(output, FileMode.OpenOrCreate, FileAccess.Write);
+                    StreamWriter sw = new StreamWriter(fs);
+                    int pos = 0;
+                    foreach (var c in charSet)
+                    {
+                        outStr += c;
+                        pos++;
+                        if (pos == 64)
+                        {
+                            outStr += "\n";
+                            pos = 0;
+                        }
+                    }
+                    byte[] unicodeBytes = unicode.GetBytes(outStr);
+                    sw.Write(outStr);
+                    fs.Close();
                 }
                 catch { }
             }
